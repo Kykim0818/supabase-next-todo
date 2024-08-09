abadf1261365f2c4c9fbefe3099009f0758ebc97
Kykim0818 <Kykim@seegene.com>
2024-08-09 10:53:26 +0900

    TODO 생성,수정,삭제(soft,hard) 함수 추가

--- apis/todos-no-rls.ts
+++ apis/todos-no-rls.ts
@@ -43,3 +43,54 @@ export const getTodosBySearch = async (terms: string) => {
 
   return result.data;
 };
+
+// todoList 생성하기
+export const createTodos = async (content: string) => {
+  const supabase = createSupabaseBrowserClient();
+  const result = await supabase
+    .from("todos_no_rls")
+    .insert({
+      content,
+    })
+    .select(); // insert 후 결과를 조회
+
+  return result.data;
+};
+
+// todoList 업데이트하기
+export const updateTodos = async (id: number, content: string) => {
+  const supabase = createSupabaseBrowserClient();
+  const result = await supabase
+    .from("todos_no_rls")
+    .update({
+      content,
+      updated_at: new Date().toISOString(), // update 날짜 갱신
+    })
+    .eq("id", id)
+    .select(); // insert 후 결과를 조회
+
+  return result.data;
+};
+
+// todoList soft Delete
+export const deleteTodosSoft = async (id: number) => {
+  const supabase = createSupabaseBrowserClient();
+  const result = await supabase
+    .from("todos_no_rls")
+    .update({
+      updated_at: new Date().toISOString(),
+      deleted_at: new Date().toISOString(), // deleted at 이 채워지면 삭제된 것으로 간주
+    })
+    .eq("id", id)
+    .select(); // insert 후 결과를 조회
+
+  return result.data;
+};
+
+// todoList Hard Delete
+// 최대한 사용을 안하는게 좋으므로 주석
+// export const deleteTodosHard = async (id: number) => {
+//   const supabase = createSupabaseBrowserClient();
+//   const result = await supabase.from("todos_no_rls").delete().eq("id", id);
+//   return result.data;
+// };
