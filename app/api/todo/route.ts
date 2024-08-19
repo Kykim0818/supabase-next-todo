414f71b3c3c7c905df69c9f2f8d63157bda992e2
Kykim0818 <Kykim@seegene.com>
2024-08-19 21:53:51 +0900

    serverAction을 활용한 예시 코드 추가 작성
    1. Client todo/Component에서 Server Action 사용
    2 .기존 Api RouterHandling 하던 부분에서 사용
    3. SSR에서 Serveraciton 사용

--- app/api/todo/route.ts
+++ app/api/todo/route.ts
@@ -1,17 +1,8 @@
-import { createServerSideClient } from "@/lib/supabase";
+import { getTodoAction } from "@/actions/todo/todo.action";
 import { NextResponse } from "next/server";
 
 export const GET = async () => {
-  const supabase = await createServerSideClient();
-
-  const result = await supabase
-    .from("todos_no_rls")
-    .select("*")
-    .is("deleted_at", null)
-    .order("id", {
-      // 내림차순 6,5,4
-      ascending: false,
-    });
+  const result = await getTodoAction();
   console.log("ping GET API income", result);
   return NextResponse.json({ ...result });
 };
