3c28fdcca6103bdeb4b28ef964188e3e53cbbbc8
Kykim0818 <Kykim@seegene.com>
2024-08-23 21:24:22 +0900

    feat: user.action getUser 함수 수정, TodoContainer에 OwnerId  전달하게 수정

--- actions/auth/user.action.ts
+++ actions/auth/user.action.ts
@@ -1,7 +1,16 @@
 import { createServerSideClient } from "@/lib/supabase";
 
-export const getUser = async () => {
-  const supabase = await createServerSideClient();
+/**
+ * 변수가 추가된 이유 (배경)
+- 2.1 서버 액션의 경우에는 서버 사이드 랜더링 도중에 호출될 수 있습니다. 이러한 환경에서는 서버는 쿠키 조작이 불가능 합니다.
+- 또 다른 경우에는요.  
+- 2.2 서버 액션은 라우터 핸들러, 혹은 미들웨어에서도 호출될 수 있습니다. 이러한 환경에서는 서버는 쿠키 조작이 가능합니다.    
+3.serverComponent 변수가 추가된 이유 (해결)  
+- 2.1 같은 환경을 분기처리하기 위해 serverComponent 변수가 추가 되었습니다.  
+- 서버 액션은 다양한 컨텍스트에서 활용될 수 있으므로 액션의 호출된 컨텍스트가 붙게되는겁니다.!  
+ */
+export const getUser = async ({ serverComponent = false }) => {
+  const supabase = await createServerSideClient(serverComponent);
   const user = await supabase.auth.getUser();
   return user?.data?.user;
 };
--- app/(site)/components/TodoContainer.tsx
+++ app/(site)/components/TodoContainer.tsx
@@ -2,13 +2,22 @@
 
 import useTodosController from "../hooks/useTodosController";
 
-export const TodoContainer = () => {
+interface TodoContainerProps {
+  ownerUserId?: string;
+}
+
+export const TodoContainer = ({ ownerUserId }: TodoContainerProps) => {
   const { loading, todos } = useTodosController();
 
   console.log(">>loading", loading);
   console.log(">>todos", todos);
 
-  return <div>TODO container</div>;
+  return (
+    <div>
+      TODO container
+      <div>Todo List</div>
+    </div>
+  );
 };
 
 export default TodoContainer;
