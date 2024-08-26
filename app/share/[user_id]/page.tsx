40c83cc9d622b76b2ac53bb388774c785c94b8cf
Kykim0818 <Kykim@seegene.com>
2024-08-26 17:21:19 +0900

    feat: Profile 조회를 통해 Share Page 이동 및 에외 처리
    - 예외시에는 Home 으로 이동

--- app/share/[user_id]/page.tsx
+++ app/share/[user_id]/page.tsx
@@ -1,3 +1,6 @@
+import { getProfileById } from "@/actions/auth/user.action";
+import { permanentRedirect } from "next/navigation";
+
 interface SharePageProps {
   params: { user_id: string };
   searchParams: {};
@@ -5,8 +8,11 @@ interface SharePageProps {
 
 const page = async (props: SharePageProps) => {
   const userId = props?.params?.user_id;
+  const profile = await getProfileById({ serverComponent: true, userId });
   console.log(">>userId", userId);
 
+  // ?
+  if (!profile) permanentRedirect("/");
   return <div>share page</div>;
 };
 
